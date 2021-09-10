840d6ee3f06078e14ab87f30cd00fd7c41133bbb
JFinkel7 <JFinkelstudios@gmail.com>
2021-09-10 10:57:41 -0500

    Added Actor Input

--- Source/CharacterDevelopment/Dummy.cpp
+++ Source/CharacterDevelopment/Dummy.cpp
@@ -5,7 +5,13 @@
 // Sets default values
 ADummy::ADummy() {
     // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-    PrimaryActorTick.bCanEverTick = true;
+    PrimaryActorTick.bCanEverTick = false;
+
+    //CollisionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("Trigger Capsule"));
+    //CollisionSphere->InitSphereRadius(55.0f);
+
+    //CollisionSphere->SetCollisionProfileName(TEXT("Trigger"));
+    //CollisionSphere->SetupAttachment(RootComponent);
 
     // [ROOT]
     _meshVisual = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RootComponent"));
@@ -15,19 +21,47 @@ ADummy::ADummy() {
     _meshVisual->SetSimulatePhysics(true);
     _meshVisual->SetEnableGravity(true);
     // - PARENT
+    _meshVisual->SetupAttachment(RootComponent);
     _meshVisual->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
 
+    ActorInputComponent = CreateDefaultSubobject<UInputComponent>("InputComponent");
 
+  
 }
 
 // Called when the game starts or when spawned
 void ADummy::BeginPlay() {
     Super::BeginPlay();
- 
+    // 
+
+
+    UWorld * world = GetWorld();
+    if(world != NULL){
+        GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Orange, TEXT("(World)-> !NULL"));
+        APlayerController* controller  = world->GetFirstPlayerController();
+        if(controller != NULL){
+            ADummy::EnableInput(controller);
+            ActorInputComponent->bBlockInput = false;
+            FInputActionBinding Binding = InputComponent->BindAction(TEXT("Test"), IE_Pressed, this, &ADummy::SaveGame);
+            Binding.bConsumeInput = false;
+            //GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Orange, TEXT("(Controller)-> !NULL"));
+            //controller->BindAction("Test", IE_Pressed, this, &ADummy::SaveGame);
+            //InputComponent->BindKey(EKeys::A, IE_Released, this, &ADummy::jumpCallback);
+            //bool isKeyDown = controller->IsInputKeyDown(EKeys::T);
+            // if(controller->WasInputKeyJustPressed(EKeys::B)){
+            //     GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Orange, TEXT("Key T Down"));
+            // }
+        }
+    }
 
     //_healthComponent->TakeDamage(this,10.00f,UDamageType::StaticClass(),NULL,NULL);
 }
 
+//=========================================[INPUT]=======================================
+
+void ADummy::SaveGame() {
+    GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Saved"));
+}
 // Called every frame
 void ADummy::Tick(float DeltaTime) {
     Super::Tick(DeltaTime);
@@ -36,14 +70,13 @@ void ADummy::Tick(float DeltaTime) {
 float ADummy::TakeDamage(float DamageAmount, struct FDamageEvent const &DamageEvent, class AController *EventInstigator, AActor *DamageCauser) {
     const float ActualDamage = Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
     GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Applying Damage!"));
-	CurrentHealth -= ActualDamage;
+    CurrentHealth -= ActualDamage;
     GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, FString::Printf(TEXT("Damage Was Taken By Actor: %f"), CurrentHealth));
 
-	if (CurrentHealth <= 0.0f){
-		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Character Is Dead"));
+    if (CurrentHealth <= 0.0f) {
+        GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Character Is Dead"));
         ADummy::Destroy();
-	}
-	
-    return ActualDamage; 
-}
+    }
 
+    return ActualDamage;
+}
--- Source/CharacterDevelopment/Dummy.h
+++ Source/CharacterDevelopment/Dummy.h
@@ -5,9 +5,11 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 //
-#include "UObject/ConstructorHelpers.h"
 #include "Components/BoxComponent.h"
 #include "Components/SceneComponent.h"
+#include "Components/SphereComponent.h"
+#include "InputCoreTypes.h"
+#include "UObject/ConstructorHelpers.h"
 //
 #include "Dummy.generated.h"
 
@@ -19,23 +21,35 @@ class CHARACTERDEVELOPMENT_API ADummy : public AActor {
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Mesh, meta = (AllowPrivateAccess = "true"))
     class UStaticMeshComponent *_meshVisual;
 
+    //! @brief: Sets Up Our Controller Input
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Mesh, meta = (AllowPrivateAccess = "true"))
+    class UInputComponent *ActorInputComponent;
+
+    /** Sphere collision component **/
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Collision, meta = (AllowPrivateAccess = "true"))
+    class USphereComponent *CollisionSphere;
 
 public:
     // Sets default values for this actor's properties
     ADummy();
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Health)
-	float MaxHealth = 100;
+    float MaxHealth = 100;
 
-	UPROPERTY(VisibleInstanceOnly, BlueprintReadWrite, Category = Health)
-	float CurrentHealth = 100;
+    UPROPERTY(VisibleInstanceOnly, BlueprintReadWrite, Category = Health)
+    float CurrentHealth = 100;
+
+
 
 protected:
     // Called when the game starts or when spawned
     virtual void BeginPlay() override;
-     
-    virtual float TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCauser) override;
 
+    virtual float TakeDamage(float DamageAmount, struct FDamageEvent const &DamageEvent, class AController *EventInstigator, AActor *DamageCauser) override;
+
+
+
+    void SaveGame();
 public:
     // Called every frame
     virtual void Tick(float DeltaTime) override;
